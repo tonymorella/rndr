81a35ef2bde888a0a73a54e20a952cbe5a6b5e9a
Gravitymaster <git@demarctech.com>
2020-10-27 19:06:19 -0400

    git update change

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,6 +1,5 @@
 @echo off
 pushd %~dp0
-PowerShell.exe -NoLogo -ExecutionPolicy Bypass -File .\kill.ps1
 git reset --hard
 git pull
 PowerShell.exe -NoLogo -ExecutionPolicy Bypass -File .\rndr_start7.ps1
